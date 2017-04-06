30e0ca9c26d00aa9c342208c8fe2b8fcf8c17561
Ryan Moore <ryan.moore@mx.com>
2017-04-06 00:02:18 -0600

    random code generation with puts and posts all the way to pg

--- src/compojure_secrets/handler.clj
+++ src/compojure_secrets/handler.clj
@@ -1,12 +1,24 @@
 (ns compojure-secrets.handler
             
-  (:require [clojure.java.jdbc :as sql]
+  (:require [clojure.string :as string] 
+            [clojure.java.jdbc :as sql]
             [compojure.route :as route]
-            [compojure.core :refer [GET POST defroutes]]
+            [compojure.core :refer [GET POST PUT defroutes]]
             [ring.util.response :refer [content-type resource-response response]]
             [ring.middleware.json :as middleware]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
             
+
+(defn code []
+  (loop [result []]
+    (if (= (count result) 4)
+      (string/join result)
+      (recur
+        (conj
+          result
+          (rand-nth
+            ["Billy" "Pilgrim" "Eichmann" "Angela" "Hoenikker" "Anita" "Elliot" "Rosewater" "Kilgore" "Trout" "Bertram" "Copeland" "Rumfoord"]))))))
+
 (def pg-db 
   {:dbtype "postgresql"
    :dbname "secrets"
@@ -18,16 +30,21 @@
 (defn get-secret-for-code []
   (sql/query pg-db "select * from secrets"))
 
-(defn add-secret [request]
+(defn add-secret [{secret :secret}]
   (sql/insert! pg-db :secrets
-    {:code "code" :secret "secret" :status "PENDING"})
-  (str request))
+    {:code (code) :secret secret :status "PENDING"})
+  (code))
 
-  ; TODO::
-  ; -> initialize secrets table with id, code, secret and reviewed columns
-  ; -> id auto incrementing
-  ; -> code, secret simple text fields
-  ; -> reviewed: enum "PENDING", "APPROVED", "REJECTED"
+(defn update-secret [{id :id status :status}]
+  (sql/update! pg-db :secrets
+    {:status status} ["id = ?" id])
+    (str status))
+
+; TODO::
+; -> initialize secrets table with id, code, secret and reviewed columns
+; -> id auto incrementing
+; -> code, secret simple text fields
+; -> reviewed: enum "PENDING", "APPROVED", "REJECTED"
 ; (defn init-db []
 ;   (sql/create-table-ddl
 ;     :secrets
@@ -38,15 +55,11 @@
 
 (defroutes app-routes
   (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
-  (GET  "/widgets" [] (response [{:name "Widget 1"} {:name "Widget 2"}]))
-  (POST "/secrets" {params :params body :body} (response body))
-  ; (GET "/secrets" [] ()
-  ;   (str "getting secrets: " (get-secret-for-code)))
-  ; (POST "/secrets" [] ()
-  ;   (str "posting secrets: " (add-secret)))
+  (GET  "/secrets" [] (response (get-secret-for-code)))
+  (POST "/secrets" {body :body} (add-secret body))
+  (PUT "/secrets" {body :body} (update-secret body))
   (route/resources "/")
   (route/not-found "Not Found"))
-
 
 (def app
   (-> app-routes
