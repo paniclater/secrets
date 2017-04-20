ada999e2cb1351c4edf9032562f347dbae26d8a9
Ryan Moore <ryan.moore@mx.com>
2017-04-20 16:13:38 -0600

    adding post on submit button click

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -1,9 +1,9 @@
 (ns compojore-secrets.core
   (:require [reagent.core :as r]
-            [ajax.core :refer [GET]]))
+            [ajax.core :refer [GET POST]]))
 (enable-console-print!)
 
-(def state (r/atom {:secrets []}))
+(def state (r/atom {:secrets [{:text "secrets haven't been fetched yet"}] :secret ""}))
 
 (defn list-secrets []
   [:ul
@@ -19,9 +19,18 @@
     {:response-format :json
       :keywords? true
       :handler handler}))
+(defn post-secret []
+  (POST "/secrets"
+    {:format :json
+     :response-format :json
+     :params {:text (:secret @state)}
+     :handler handler}))
 
 (defn clear-secrets []
   (swap! state (fn [a] (assoc a :secrets []))))
+
+(defn update-secret [event]
+  (swap! state (fn [a] (assoc a :secret (-> event .-target .-value)))))
 
 (defn app []
   [:div
@@ -30,11 +39,13 @@
     [:button {:on-click get-secrets} "Get Secrets"]
     [:button {:on-click clear-secrets} "Clear Secrets"]
     [:div
-      [:input.form-control]]])
+      [:textarea {:rows 3 :placeholder "enter a secret" :value (:secret @state) :on-change update-secret}]
+      [:p (:secret @state)]
+      [:p "hello"]
+      [:button {:on-click post-secret} "Submit Secret"]]])
 
 
 (defn render-simple []
-  ; (get-secrets)
     (r/render [app]
       (.getElementById js/document "root")))
 
