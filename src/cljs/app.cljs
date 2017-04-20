781e385e711efce478348668c97f9dc5a29d74c6
Ryan Moore <ryan.moore@mx.com>
2017-04-20 14:20:16 -0600

    first reagent components

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -1,24 +1,41 @@
 (ns compojore-secrets.core
   (:require [reagent.core :as r]
             [ajax.core :refer [GET]]))
-
 (enable-console-print!)
 
-(defn print-secrets [ok response]
-  (if ok
-    response
-    (.error js/console (str response))))
+(def state (r/atom {:secrets []}))
+
+(defn list-secrets []
+  [:ul
+    (for [secret (:secrets @state)]
+      ^{:key secret} [:li (:text secret)])])
+
+(defn handler [response]
+  (swap! state #(assoc % :secrets response)))
+  ; (swap! state (fn [a] (assoc a :secrets response))))
 
 (defn get-secrets []
-  (GET "/secrets" {:response-format :json :handler print-secrets}))
+  (GET "/secrets"
+    {:response-format :json
+      :keywords? true
+      :handler handler}))
 
-; (defn secrets-list []
-;   [:ul
-;    (for [item items])])
+(defn clear-secrets []
+  (swap! state (fn [a] (assoc a :secrets []))))
+
+(defn app []
+  [:div
+    [:h2 "secrets"]
+    [list-secrets]
+    [:button {:on-click get-secrets} "Get Secrets"]
+    [:button {:on-click clear-secrets} "Clear Secrets"]
+    [:div
+      [:input.form-control]]])
+
 
 (defn render-simple []
-  (r/render [:div [:p "hello world"]]
-    (.getElementById js/document "root")))
+  ; (get-secrets)
+    (r/render [app]
+      (.getElementById js/document "root")))
 
 (render-simple)
-
