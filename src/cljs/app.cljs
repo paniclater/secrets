10783ce26f8519f459aeb88921e03555cb98bde3
Ryan Moore <ryan.moore@mx.com>
2017-06-12 20:05:50 -0600

    removing unused listing secrets handlers

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -8,34 +8,19 @@
  ;;4. Routes?
  ;;5. Styling - [ ]
 
-(ns compojore-secrets.core
+(ns secrets.core
   (:require [reagent.core :as r]
             [ajax.core :refer [GET POST]]))
 (enable-console-print!)
-
-(def state (r/atom {:secrets [{:text "secrets haven't been fetched yet"}]
-                    :secret ""
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; STATE
+(def state (r/atom {:secret ""
                     :code ""
                     :prompt "Send me a secret and if it is deemed worthy you will be rewarded with the luscious sounds of Agatha Frisky"}))
 
-(defn list-secrets []
-  [:ul
-    (for [secret (:secrets @state)]
-      ^{:key secret} [:li (:text secret)])])
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HTTP REQUESTS AND HANDLERS
 (defn post-secret-handler [response] (swap! state #(assoc % :prompt (concat "Your code " (:code response)))))
-(defn get-music-handler [response] (println (:headers response)))
-
-(defn get-secrets-handler [response]
-  (swap! state #(assoc % :secrets response)))
-
-
-(defn get-secrets []
-  (GET "/secrets"
-    {:response-format :json
-      :keywords? true
-      :handler get-secrets-handler}))
-
 (defn post-secret []
   (POST "/secrets"
     {:format :json
@@ -44,15 +29,7 @@
      :params {:text (:secret @state)}
      :handler post-secret-handler}))
 
-(defn clear-secrets []
-  (swap! state (fn [a] (assoc a :secrets []))))
-
-(defn update-secret [event]
-  (swap! state (fn [a] (assoc a :secret (-> event .-target .-value)))))
-
-(defn update-code [event]
-  (swap! state (fn [a] (assoc a :code (-> event .-target .-value)))))
-
+(defn get-music-handler [response] (println response))
 (defn get-music []
   (let [url (str "secrets/" (:code @state))]
     (GET url
@@ -61,23 +38,26 @@
        :keywords? true
        :handler get-music-handler})))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; APP STATE HANDLERS
+
+(defn update-secret [event]
+  (swap! state (fn [a] (assoc a :secret (-> event .-target .-value)))))
+(defn update-code [event]
+  (swap! state (fn [a] (assoc a :code (-> event .-target .-value)))))
+
 (defn app []
   [:div
     [:h2 "secrets"]
-    [:p (:prompt @state)]
-    [:button {:on-click get-secrets} "Get Secrets"]
-    [:button {:on-click clear-secrets} "Clear Secrets"]
     [:div
-      [:textarea {:rows 3 :placeholder "enter a secret" :value (:secret @state) :on-change update-secret}]
-      [:p (:secret @state)]
+      [:textarea {:rows 30 :placeholder "enter a secret" :value (:secret @state) :on-change update-secret}]
       [:button {:on-click post-secret} "Submit Secret"]]
     [:div
       [:input {:placeholder "enter a code" :value (:code @state) :on-change update-code}]
-      [:p (:code @state)]
       [:button {:on-click get-music}  "Get Music By Code"]]])
 
-(defn render-simple []
+(defn render-app []
     (r/render [app]
       (.getElementById js/document "root")))
 
-(render-simple)
+(render-app)
