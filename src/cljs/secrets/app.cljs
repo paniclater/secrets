0cdb7c26bb7cd64dd076bf0f091ce37a92363c80
Ryan Moore <ryan.moore@mx.com>
2017-06-20 09:50:45 -0600

    moving parts into components

--- src/cljs/secrets/app.cljs
+++ src/cljs/secrets/app.cljs
@@ -16,7 +16,8 @@
 ;; STATE
 (def state (r/atom {:secret ""
                     :code ""
-                    :prompt "they sends the code, we sends the response"}))
+                    :prompt "they sends the code, we sends the response"
+                    :show-download-link false}))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HTTP REQUESTS AND HANDLERS
@@ -29,17 +30,17 @@
      :params {:text (:secret @state)}
      :handler post-secret-handler}))
 
-(defn update-prompt [new-prompt] (swap! state #(assoc % :prompt new-prompt)))
 (defn check-secret-status-handler [response]
   (cond
     (= (:status response) "PENDING") (update-prompt "their secrets has not been weighed yet")
-    (= (:status response) "APPROVED") (update-prompt "their secrets has been found worthy!")))
+    (= (:status response) "APPROVED") (update-prompt "their secrets has been found worthy!")
+    :else (update-prompt "Uh oh, something went wrong, please email ryan@paniclater.com")))
 
 (defn check-secret-status-error-handler [response]
   (let [status (:status response)]
-    (cond
-      (= status 402) (update-prompt "their secrets was weighed and found wanting")
-      :else (println "code not found"))))
+    (if (= status 402)
+      (update-prompt "their secrets was weighed and found wanting")
+      (update-prompt "Uh oh, something went wrong, please email ryan@paniclater.com"))))
 
 (defn check-secret-status []
   (let [url (str "secrets/" (:code @state))]
@@ -56,7 +57,32 @@
   (swap! state #(assoc % :secret (-> event .-target .-value))))
 (defn update-code [event]
   (swap! state #(assoc % :code (-> event .-target .-value))))
+(defn toggle-show-download-link [shouldShow]
+  (swap! state #(assoc & :show-download-link shouldShow)))
+(defn update-prompt [new-prompt]
+  (swap! state #(assoc % :prompt new-prompt)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; REAGENT COMPONENTS
+(defn secret-submitter []
+  [:div
+    [:textarea {:rows 30 :placeholder "enter a secret" :value (:secret @state) :on-change update-secret}]
+    [:button {:on-click post-secret} "Submit Secret"]])
+
+(defn secret-checker []
+  [:div
+    [:p (:prompt @state)]
+    [:input {:placeholder "enter a code" :value (:code @state) :on-change update-code}]
+    [:button {:on-click check-secret-status}  "Check status by entering code"]])
+
+(defn download-link []
+  [:div
+    [:a
+     {:href (str "music/" (:code @state) "/agatha-frisky.zip")}
+     "Click Here To Download!"]])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PARENT COMPONENT
 (defn app []
   [:div
     [:h2 "gives a secrets, gets a musics"]
@@ -64,17 +90,13 @@
     [:p "we waits, we reads the secret, we rates the secret"]
     [:p "they comes back, they writes the code, they clicks the send, if the secret is tasty, we sends the music"]
     [:hr]
-    [:div
-      [:textarea {:rows 30 :placeholder "enter a secret" :value (:secret @state) :on-change update-secret}]
-      [:button {:on-click post-secret} "Submit Secret"]]
+    [secret-submitter]
     [:hr]
-    [:div
-      [:p (:prompt @state)]
-      [:input {:placeholder "enter a code" :value (:code @state) :on-change update-code}]
-      [:button {:on-click check-secret-status}  "Check status by entering code"]]
-    [:div
-      [:a {:href (str  "/music/" (:code @state) "/agatha-frisky.zip")} "You're Approved! Click Here To Download!"]]])
+    [secret-checker]
+    [download-link]])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; REAGENT MOUNT
 (defn render-app []
     (r/render [app]
       (.getElementById js/document "root")))
