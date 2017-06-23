683202ca2199377bfb1ef7b6ef7d03c04ed28979
Ryan Moore <ryan.moore@mx.com>
2017-06-22 23:35:16 -0600

    key ico

--- src/clj/secrets/handler.clj
+++ src/clj/secrets/handler.clj
@@ -85,6 +85,7 @@
       [:head
         [:meta {:charset "utf-8"}]
         [:meta {:name "viewport" :content "width=device-width"}]
+        [:link {:rel "icon" :href "/key.ico" :type "image/x-icon"}]
         [:title "Agatha Frisky Exchanges Music For Secrets"]]
       [:div#root.container [:h3 "doing some html"]]
       (include-css "/index.css")
