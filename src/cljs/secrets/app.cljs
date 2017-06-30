a22d067a0d6f674e7ccf82253f33c1ea3ac671c0
Ryan Moore <ryan.moore@mx.com>
2017-06-29 22:11:06 -0600

    don't nest anchor tags in buttons

--- src/cljs/secrets/app.cljs
+++ src/cljs/secrets/app.cljs
@@ -71,11 +71,10 @@
 
 (defn download-link []
   [:div
-   [:button.download-button
-     [:a.download-link
-       {:href (str "https://secrets.audio/music/" (:code @state) "/Tarred_and_Pleasured_By_Agatha_Frisky.zip")
-        :target "_blank"}
-       "Your Secret is Approved, Click Here To Download!"]]])
+    [:a.download-link
+     {:href (str "music/" (:code @state) "/Tarred_and_Pleasured_By_Agatha_Frisky.zip")
+      :target "_blank"}
+     "Your Secret is Approved, Click Here To Download!"]])
 
 (defn header []
   [:div.secrets-app__header
